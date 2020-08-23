279138a0a0843abfbb73a0e05386e65e2acb1683
Incheol Jung <bluesky761@naver.com>
2020-08-24 02:21:50 +0900

    NPE 케이스 추가

--- src/main/java/com/example/practice/npe/NPEExample.java
+++ src/main/java/com/example/practice/npe/NPEExample.java
@@ -4,6 +4,11 @@ package com.example.practice.npe;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import lombok.NonNull;
 
 /**
@@ -53,6 +58,20 @@ public class NPEExample {
         }
     }
 
+    public void optionalTest(Optional<String> test){
+        if(test.isPresent()){
+            System.out.println(test.toString());
+        }
+    }
+
+    private void tryCatchTest(String test) {
+        try {
+            System.out.println(test.toString());
+        }catch (Exception ex){
+            System.out.println("catch - null pointer exception");
+        }
+    }
+
     public static void main(String[] args) {
         NPEExample example = new NPEExample();
         // case 1. @NonNull 사용
@@ -68,6 +87,30 @@ public class NPEExample {
 //        System.out.println(example.valueOfTest(null));
 
         // case 5. null 체크, 또는 StringUtils 사용
-        example.nullCheckString(null);
+//        example.nullCheckString(null);
+
+        // case 6. optional 사용
+        // Optional.of(T)
+        // - T로 받은 non-null 값을 포함하는 Optional 객체 반환, T가 null 일 경우 NPE 발생
+        // Optional.absent()
+        // - 아무것도 포함하고 있지 않는 absent Optional 객체 반환
+        // Optional.fromNullable(T)
+        // - T로 받은 값이 non-null일 경우 present로, null일 경우 absent로 처리한 Optional 객체 반환
+//        String tempString = null;
+//        Optional<String> optionalString = Optional.ofNullable(tempString);
+//        example.optionalTest(optionalString);
+
+        // case 7. try / catch 사용
+//        example.tryCatchTest(null);
+
+        // case 8. method chainging 피하기
+//        exmaple.getPerson().getAge().getYear();
+
+        // case 9. List는 null 보다는 초기화 또는 emptyList
+        List<Integer> items = null;
+        items = new ArrayList<Integer>();
+        items = Collections.emptyList();
     }
+
+
 }
